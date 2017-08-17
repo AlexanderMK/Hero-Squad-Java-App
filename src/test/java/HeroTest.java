18477e18780321a3e1c052b81f7b3399a4fb7a94
Alexander <mburukungu@gmail.com>
2017-08-17 11:10:05 +0300

    test1 - checked if we can instatiate Hero objects

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.*;
 public class HeroTest {
 
   @Test
+	public void Hero_instantiatesCorrectly_true() {
+		assertTrue(testHero instanceof Hero);
+	}
+
+  @Test
 	public void getName_returnsName_NinjaMan() {
 		assertEquals("NinjaMan", testHero.getName());
 	}
