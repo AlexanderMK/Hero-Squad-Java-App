0d3f8f8c057901528a0f2815136e30b99e951f8b
Alexander <mburukungu@gmail.com>
2017-08-17 12:03:06 +0300

    placed get request to retrieve heros from the session

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,9 +18,16 @@ public class App {
     post("/heros", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       String name = request.queryParams("name");
-      Task newTask = new Task(description);
+      Hero newHero = new Hero(name);
       request.session().attribute("hero", newHero);
       model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("hero", request.session().attribute("hero"));
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
