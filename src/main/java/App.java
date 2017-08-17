af6265f666c09f5524a367e8fb8aab0446aa6cc3
Alexander <mburukungu@gmail.com>
2017-08-17 15:08:38 +0300

    imported Arraylist package at the top

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,9 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
+import java.util.ArrayList;
+
+
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
@@ -11,24 +14,29 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("heros", request.session().attribute("heros"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     post("/heros", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+
+      ArrayList<Hero> heros = request.session().attribute("heros");
+      if (heros == null) {
+      heros = new ArrayList<Hero>();
+      request.session().attribute("heros", heros);
+      }
+
       String name = request.queryParams("name");
       Hero newHero = new Hero(name);
-      request.session().attribute("hero", newHero);
+      heros.add(newHero);
+
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("hero", request.session().attribute("hero"));
-      model.put("template", "templates/index.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+
+
   }
 }
