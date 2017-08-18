03288cc2fa52799de24810a5c85ad499d867b4ec
Alexander <mburukungu@gmail.com>
2017-08-18 15:17:36 +0300

    placed logic for maxSize

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,8 +1,10 @@
 public class Squad {
   private String mName;
+  private int mMaxSize;
 
   public Squad(String name) {
     mName = name;
+    mMaxSize = maxSize;
   }
 
   public String getName() {
