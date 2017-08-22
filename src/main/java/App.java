f418c7e5d255b580d05b80f22f381cae938bcb45
Alexander <mburukungu@gmail.com>
2017-08-22 09:52:43 +0300

    pusheed to heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,16 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
+
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
@@ -119,6 +129,6 @@ public class App {
 			return new ModelAndView(model, layout);
 		}, new VelocityTemplateEngine());
 
-  
+
   }
 }
